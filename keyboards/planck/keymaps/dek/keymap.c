85fd48e2b970cb6c823baa30115b2cc9151a916a
Cédric Laudrel <dek@iono.me>
2017-01-22 18:50:04 +0100

    planck : add numpad layer

--- keyboards/planck/keymaps/dek/keymap.c
+++ keyboards/planck/keymaps/dek/keymap.c
@@ -17,12 +17,14 @@ extern keymap_config_t keymap_config;
 #define _QWERTY 0
 #define _LOWER 1
 #define _RAISE 2
+#define _NUMPAD 3
 #define _ADJUST 16
 
 enum planck_keycodes {
   QWERTY = SAFE_RANGE,
   LOWER,
   RAISE,
+  NUMPAD,
   BACKLIT,
   EXT_PLV
 };
@@ -48,7 +50,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   {KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC},
   {KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_MINS, KC_ENT},
   {KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_QUOT, KC_RSFT},
-  {KC_LCTL, KC_LGUI, KC_NO,   KC_LALT, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_SCLN, KC_SLSH, KC_BSLS,   KC_RCTL}
+  {KC_LCTL, KC_LGUI, NUMPAD,  KC_LALT, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_SCLN, KC_SLSH, KC_BSLS,   KC_RCTL}
 },
 /* Lower
  * ,-----------------------------------------------------------------------------------.
@@ -84,6 +86,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   {KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_LEFT,   KC_DOWN, KC_UP,  KC_RGHT, KC_EQL, KC_TRNS},
   {_______, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_TRNS, KC_LPRN, KC_RPRN, _______},
   {_______, _______, _______, _______, _______, _______, _______, _______, KC_TRNS, KC_LCBR, KC_RCBR, KC_TRNS}
+},
+[_NUMPAD] = {
+  {_______, _______, _______, _______, _______, _______, _______, _______, _______, KC_7, KC_8, KC_9},
+  {_______, _______, _______, _______, _______, _______, _______, _______, _______, KC_4, KC_5,  KC_6},
+  {_______, _______, _______, _______, _______, _______, _______,  _______, _______,KC_1, KC_2, KC_3},
+  {_______, _______, _______, _______, _______, _______, _______, _______, _______, KC_0, KC_DOT, _______}
 },
 /* Adjust (Lower + Raise)
  * ,-----------------------------------------------------------------------------------.
@@ -140,6 +148,14 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       } else {
         layer_off(_RAISE);
         update_tri_layer(_LOWER, _RAISE, _ADJUST);
+      }
+      return false;
+      break;
+    case NUMPAD:
+      if (record->event.pressed) {
+        layer_on(_NUMPAD);
+      } else {
+        layer_off(_NUMPAD);
       }
       return false;
       break;
