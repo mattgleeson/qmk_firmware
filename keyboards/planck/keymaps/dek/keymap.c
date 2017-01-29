f64b082942fb85b7fb33d36dcc1305295f3db94b
Cédric Laudrel <dek@iono.me>
2017-01-29 15:22:58 +0100

    planck : add curly braces below brackets

--- keyboards/planck/keymaps/dek/keymap.c
+++ keyboards/planck/keymaps/dek/keymap.c
@@ -84,7 +84,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_RAISE] = {
   {KC_GRV,  KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR,  KC_LPRN, KC_RPRN, KC_TRNS},
   {KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_DOWN, KC_UP,  KC_LBRC, KC_RBRC, KC_TRNS},
-  {_______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_TRNS, KC_TRNS, KC_LPRN, KC_RPRN, _______},
+  {_______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_TRNS, KC_TRNS, KC_LCBR, KC_RCBR, _______},
   {_______, _______, _______, _______, _______, _______, _______, _______, KC_TRNS, KC_EQL, KC_TRNS, KC_TRNS}
 },
 [_NUMPAD] = {
