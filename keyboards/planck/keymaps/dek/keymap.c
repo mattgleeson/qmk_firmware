10418b79eb8e0800dee4cd18688b1a48e54cfdae
Cédric Laudrel <dek@iono.me>
2017-02-19 14:16:54 +0100

    rework dek planck layout

--- keyboards/planck/keymaps/dek/keymap.c
+++ keyboards/planck/keymaps/dek/keymap.c
@@ -48,9 +48,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_QWERTY] = {
   {KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC},
-  {KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_QUOT, KC_ENT},
-  {KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, MT(MOD_RSFT,KC_MINS)},
-  {KC_LCTL, KC_LGUI, KC_LALT, NUMPAD,  LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_SCLN, KC_MINS, KC_BSLS,   KC_RCTL}
+  {KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT},
+  {KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT},
+  {KC_LCTL, KC_LGUI, KC_LALT, NUMPAD,  LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_RALT, KC_RGUI, KC_RSFT,   KC_RCTL}
 },
 /* Lower
  * ,-----------------------------------------------------------------------------------.
@@ -65,9 +65,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_LOWER] = {
   {KC_TRNS, KC_1, KC_2,   KC_3, KC_4,  KC_5, KC_6, KC_7, KC_8,    KC_9,    KC_0,    KC_DEL},
-  {KC_TAB,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_LEFT,   KC_DOWN, KC_UP,    KC_RGHT,    KC_GRV,    KC_TRNS},
+  {KC_TAB,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_LEFT,   KC_DOWN, KC_UP,    KC_RGHT,    KC_GRV,    KC_BSLS},
   {_______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_HOME,KC_PGDN,KC_PGUP,    KC_END,    KC_TRNS, _______},
-  {_______, _______, _______, KC_RALT, _______, _______, _______, _______, KC_0, KC_LBRC, KC_RBRC, KC_TRNS}
+  {_______, _______, _______, KC_RALT, _______, _______, _______, _______, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS}
 },
 
 /* Raise
@@ -82,10 +82,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_RAISE] = {
-  {KC_GRV,  KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR,  KC_LPRN, KC_RPRN, KC_TRNS},
-  {KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_DOWN, KC_UP,  KC_LBRC, KC_RBRC, KC_TRNS},
+  {KC_GRV,  KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR,  KC_LPRN, KC_RPRN, KC_EQL},
+  {KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_DOWN, KC_UP,  KC_LBRC, KC_RBRC, KC_MINS},
   {_______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_TRNS, KC_TRNS, KC_LCBR, KC_RCBR, _______},
-  {_______, _______, _______, _______, _______, _______, _______, _______, KC_TRNS, KC_EQL, KC_TRNS, KC_TRNS}
+  {_______, _______, _______, _______, _______, _______, _______, _______, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS}
 },
 [_NUMPAD] = {
   {_______, _______, _______, _______, _______, _______, _______, _______, _______, KC_7, KC_8, KC_9},
