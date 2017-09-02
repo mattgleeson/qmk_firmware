87a435b20195760f8c02affd98171bd1c26c9f8f
Cédric Laudrel <dek@iono.me>
2017-09-02 02:34:06 +0200

    dek : add lights to let's split

--- keyboards/lets_split/keymaps/dek/config.h
+++ keyboards/lets_split/keymaps/dek/config.h
@@ -32,3 +32,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #ifdef SUBPROJECT_rev2fliphalf
 	#include "../../rev2fliphalf/config.h"
 #endif
+
+#undef RGBLED_NUM
+#define RGBLIGHT_ANIMATIONS
+#define RGBLED_NUM 10
--- keyboards/lets_split/keymaps/dek/keymap.c
+++ keyboards/lets_split/keymaps/dek/keymap.c
@@ -57,7 +57,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_ADJUST] = KEYMAP( \
   _______, RESET,   _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_VOLU, \
   _______, _______, _______, AU_ON,   AU_OFF,  AG_NORM, AG_SWAP, _______, _______, _______, _______,  KC_VOLD, \
-  _______, MUV_DE,  MUV_IN,  MU_ON,   MU_OFF,  MI_ON,   MI_OFF,  _______, _______, _______, _______, _______, \
+  _______, RGB_TOG,  RGB_MOD,  RGB_HUI,  RGB_HUD,  RGB_SAI,  RGB_SAD,  RGB_VAI,  RGB_VAD, _______, _______, _______, \
   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______ \
   )
 
