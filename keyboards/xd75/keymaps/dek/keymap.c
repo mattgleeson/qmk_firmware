90fe91e1c6b2350608710a45cfd5396b4887bd69
Cédric Laudrel <dek@iono.me>
2017-09-05 00:58:41 +0200

    xd75/dek : add initial keymap

---
+++ keyboards/xd75/keymaps/dek/keymap.c
@@ -0,0 +1,75 @@
+// this layout is visible here : http://www.keyboard-layout-editor.com/#/gists/ba882ca8760bb2ca082b4eb7b00b448c
+
+#include QMK_KEYBOARD_H
+#include "action_layer.h"
+#include "eeconfig.h"
+
+extern keymap_config_t keymap_config;
+
+// Each layer gets a name for readability, which is then used in the keymap matrix below.
+// The underscores don't mean anything - you can have a layer called STUFF or any other name.
+// Layer names don't all need to be of the same length, obviously, and you can also skip them
+// entirely and just use numbers.
+#define _QWERTY 0
+#define _FN 1
+
+enum custom_keycodes {
+  QWERTY = SAFE_RANGE,
+  FN 
+};
+
+// Fillers to make layering more clear
+#define _______ KC_TRNS
+#define XXXXXXX KC_NO
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+
+[_QWERTY] = {
+  { KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    RESET,   KC_HOME, _______, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSLS },
+  { KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_LBRC, KC_PGUP, KC_RBRC, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC },
+  { KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_EQL,  KC_PGDN, KC_MINS, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_RCTL },
+  { KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_GRV,  KC_END,  KC_QUOT, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT },
+  { _______, KC_LGUI, KC_LALT, _______, FN,      KC_BSPC, KC_TAB,  _______, KC_ENT,  KC_SPC,  FN,      _______, KC_RALT, KC_RGUI, _______ },
+},
+
+[_FN] = {
+  { _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   _______,   RESET, _______, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  _______ },
+  { _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_DEL  },
+  { _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______ },
+  { _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______ },
+  { _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY },
+},
+
+};
+
+#ifdef AUDIO_ENABLE
+float tone_qwerty[][2]     = SONG(QWERTY_SOUND);
+#endif
+
+void persistent_default_layer_set(uint16_t default_layer) {
+  eeconfig_update_default_layer(default_layer);
+  default_layer_set(default_layer);
+}
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  switch (keycode) {
+    case QWERTY:
+      if (record->event.pressed) {
+        #ifdef AUDIO_ENABLE
+          PLAY_SONG(tone_qwerty);
+        #endif
+        persistent_default_layer_set(1UL<<_QWERTY);
+      }
+      return false;
+      break;
+    case FN:
+      if (record->event.pressed) {
+        layer_on(_FN);
+      } else {
+        layer_off(_FN);
+      }
+      return false;
+      break;
+  }
+  return true;
+}
