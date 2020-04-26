27ba3bd479f41bd058d901f11f3dc7d34d1c3d91
Matt Gleeson <mggleeso@amazon.com>
2020-04-25 17:22:27 -0700

    basic macroboard setup

--- keyboards/40percentclub/gherkin/keymaps/mg/keymap.c
+++ keyboards/40percentclub/gherkin/keymaps/mg/keymap.c
@@ -2,9 +2,14 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
+  [1] = LAYOUT_ortho_3x10(
+    KC_A, KC_B, KC_C, KC_D, KC_E, KC_F, KC_G,    KC_H,    KC_I,    KC_J,
+    KC_K, KC_L, KC_M, KC_N, KC_O,     KC_P,     KC_Q,    KC_R,    KC_S,    KC_T,
+    KC_U,KC_V,KC_W,KC_X, KC_Y, KC_Z,  KC_1,    KC_2,    KC_3,  RESET
+    ),
   [0] = LAYOUT_ortho_3x10(
-    KC_F1, KC_F2, KC_F3, KC_F4, KC_LBRACKET, KC_RBRACKET, KC_VOLU,    KC_PGUP,    RESET,    KC_HOME,
-    KC_F5, KC_F6, KC_F7, KC_F8, KC_MPLY,     KC_MNXT,     KC_VOLD,    KC_PGDN,    KC_UP,    KC_END,
+    KC_F1, KC_F2, KC_F3, KC_F4, KC_LBRACKET, KC_RBRACKET, KC_VOLU,    KC_HOME,    RESET,    KC_PGUP,
+    KC_F5, KC_F6, KC_F7, KC_F8, KC_MPLY,     KC_MNXT,     KC_VOLD,    KC_END,    KC_UP,    KC_PGDN,
     KC_F9,KC_F10,KC_F11,KC_F12, KC_NONUS_HASH, KC_BSLASH,  _______,    KC_LEFT,    KC_DOWN,  KC_RIGHT
   )
 };
