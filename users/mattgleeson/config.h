8477ba2afc466f85c9b37fb9f753d533cb459387
Matt Gleeson <mggleeso@amazon.com>
2020-05-07 12:50:19 -0700

    pull in 12X4

--- users/mattgleeson/config.h
+++ users/mattgleeson/config.h
@@ -17,21 +17,21 @@
 
 #define MG_1_LH_R1 KC_ESC, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5
 #define MG_1_RH_R1 KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11
-#define MG_1_LH_R2 KC_ , KC_EXLM, KC_AT, KC_LCBR, KC_RCBR, KC_PIPE
+#define MG_1_LH_R2 _______, KC_EXLM, KC_AT, KC_LCBR, KC_RCBR, KC_PIPE
 #define MG_1_RH_R2 KC_UP, KC_7, KC_8, KC_9, KC_ASTR, KC_F12
-#define MG_1_LH_R3 KC_, KC_, KC_DLR, KC_LPRN, KC_RPRN, KC_GRAVE
-#define MG_1_RH_R3 KC_DOWN, KC_4, KC_5, KC_6, KC_, KC_
-#define MG_1_LH_R4 KC_, KC_PERCENT, KC_CIRCUMFLEX, KC_LBRACKET, KC_RBRACKET, KC_TILD
-#define MG_1_RH_R4 KC_AMPR, KC_1, KC_2, KC_3, KC_BSLASH, KC_
+#define MG_1_LH_R3 _______, _______, KC_DLR, KC_LPRN, KC_RPRN, KC_GRAVE
+#define MG_1_RH_R3 KC_DOWN, KC_4, KC_5, KC_6, _______, _______
+#define MG_1_LH_R4 _______, KC_PERCENT, KC_CIRCUMFLEX, KC_LBRACKET, KC_RBRACKET, KC_TILD
+#define MG_1_RH_R4 KC_AMPR, KC_1, KC_2, KC_3, KC_BSLASH, _______
 
-#define MG_2_LH_R1 KC_, KC_, KC_, KC_, KC_, KC_
-#define MG_2_RH_R1 KC_, KC_, KC_, KC_, KC_, KC_
-#define MG_2_LH_R2 KC_, KC_, KC_, KC_MS_UP, KC_, KC_
-#define MG_2_RH_R2 KC_END, KC_PGDN, KC_PGUP, KC_HOME, KC_, KC_
-#define MG_2_LH_R3 KC_, KC_, KC_MS_LEFT, KC_MS_DOWN, KC_MS_RIGHT, KC_
-#define MG_2_RH_R3 KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT, KC_, KC_
-#define MG_2_LH_R4 KC_, KC_, KC_, KC_, KC_, KC_
-#define MG_2_RH_R4 KC_, KC_AUDIO_VOL_DOWN, KC_AUDIO_VOL_UP, KC_MEDIA_PLAY_PAUSE, KC_MEDIA_NEXT_TRACK, KC_
+#define MG_2_LH_R1 _______, _______, _______, _______, _______, _______
+#define MG_2_RH_R1 _______, _______, _______, _______, _______, _______
+#define MG_2_LH_R2 _______, _______, _______, KC_MS_UP, _______, _______
+#define MG_2_RH_R2 KC_END, KC_PGDN, KC_PGUP, KC_HOME, _______, _______
+#define MG_2_LH_R3 _______, _______, KC_MS_LEFT, KC_MS_DOWN, KC_MS_RIGHT, _______
+#define MG_2_RH_R3 KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT, _______, _______
+#define MG_2_LH_R4 _______, _______, _______, _______, _______, _______
+#define MG_2_RH_R4 _______, KC_AUDIO_VOL_DOWN, KC_AUDIO_VOL_UP, KC_MEDIA_PLAY_PAUSE, KC_MEDIA_NEXT_TRACK, _______
 
 #define MG_12X4 MG_LH_R1, MG_RH_R1, MG_LH_R2, MG_RH_R2, MG_LH_R3, MG_RH_R3, MG_LH_R4, MG_RH_R4
 #define MG_1_12X4 MG_1_LH_R1, MG_1_RH_R1, MG_1_LH_R2, MG_1_RH_R2, MG_1_LH_R3, MG_1_RH_R3, MG_1_LH_R4, MG_1_RH_R4
