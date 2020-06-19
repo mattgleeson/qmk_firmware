94b0354c600e1ec79f0d647c9ac0746f03633727
Matt Gleeson <mggleeso@amazon.com>
2020-06-19 14:25:12 -0700

    factor out ALH/ARH bits for Atreus

--- keyboards/keyboardio/atreus/keymaps/mattgleeson/keymap.c
+++ keyboards/keyboardio/atreus/keymaps/mattgleeson/keymap.c
@@ -14,34 +14,22 @@ enum layer_names {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_QW] = LAYOUT_wrapper( /* Qwerty */
-    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P    ,
-    MG_HOME_L,                                                     MG_HOME_R,
-    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_GRV,  KC_BSLS, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH ,
-    /*    KC_ESC, KC_TAB, KC_LGUI,  KC_LSFT, KC_BSPC,  KC_LCTL, KC_LALT, KC_SPC,  MO(_RS), KC_MINS, KC_QUOT, KC_ENT */
+    MG_ALH_R2,                      MG_ARH_R2,
+    MG_ALH_R3,                      MG_ARH_R3,
+    MG_ALH_R4, KC_GRAVE, KC_BSLASH, MG_ARH_R4,
     KC_ESC, KC_DEL, MG_LTHUMB_IN, MG_LTHUMB_NEAR, MG_LTHUMB_FAR, KC_MINS,
     /* */                     KC_QUOT, MG_RTHUMB_FAR, MG_RTHUMB_NEAR, MG_RTHUMB_IN, KC_RGUI, KC_EQL
   ),
-
-  /*
-   *  !       @     up     {    }        ||     pgup    7     8     9    *
-   *  #     left   down  right  $        ||     pgdn    4     5     6    +
-   *  [       ]      (     )    &        ||       `     1     2     3    \
-   * lower  insert super shift bksp ctrl || alt space   fn    .     0    =
-   */
-  [_RS] = LAYOUT( /* [> RAISE <] */
-    KC_EXLM, KC_AT,   KC_UP,   KC_LCBR, KC_RCBR,                   KC_PGUP, KC_7,    KC_8,   KC_9, KC_ASTR ,
-    KC_HASH, KC_LEFT, KC_DOWN, KC_RGHT, KC_DLR,                    KC_PGDN, KC_4,    KC_5,   KC_6, KC_PLUS ,
-    KC_LBRC, KC_RBRC, KC_LPRN, KC_RPRN, KC_AMPR, _______, _______, KC_GRV,  KC_1,    KC_2,   KC_3, KC_BSLS ,
-    TG(_LW), KC_INS,  KC_LGUI, KC_LSFT, KC_BSPC, KC_LCTL, KC_LALT, KC_SPC,  KC_TRNS, KC_DOT, KC_0, KC_EQL  ),
-  /*
-   * insert home   up  end   pgup       ||      up     F7    F8    F9   F10
-   *  del   left  down right pgdn       ||     down    F4    F5    F6   F11
-   *       volup             reset      ||             F1    F2    F3   F12
-   *       voldn  super shift bksp ctrl || alt space   L0  prtsc scroll pause
-   */
-  [_LW] = LAYOUT( /* [> LOWER <] */
-    KC_INS,  KC_HOME, KC_UP,   KC_END,  KC_PGUP,                   KC_UP,   KC_F7,   KC_F8,   KC_F9,   KC_F10  ,
-    KC_DEL,  KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN,                   KC_DOWN, KC_F4,   KC_F5,   KC_F6,   KC_F11  ,
-    KC_NO,   KC_VOLU, KC_NO,   KC_NO,   RESET,   _______, _______, KC_NO,   KC_F1,   KC_F2,   KC_F3,   KC_F12  ,
-    KC_NO,   KC_VOLD, KC_LGUI, KC_LSFT, KC_BSPC, KC_LCTL, KC_LALT, KC_SPC,  TO(_QW), KC_PSCR, KC_SLCK, KC_PAUS )
+  [_RS] = LAYOUT_wrapper(
+    MG_1_ALH_R2,                   MG_1_ARH_R2,
+    MG_1_ALH_R3,                   MG_1_ARH_R3,
+    MG_1_ALH_R4, _______, _______, MG_1_ARH_R4,
+    TG(_LW), KC_INS,  KC_LGUI, KC_LSFT, KC_BSPC, KC_LCTL, KC_LALT, KC_SPC,  KC_TRNS, KC_DOT, KC_0, KC_EQL
+  ),
+  [_LW] = LAYOUT_wrapper(
+    MG_2_ALH_R2,                   MG_2_ARH_R2,
+    MG_2_ALH_R3,                   MG_2_ARH_R3,
+    MG_2_ALH_R4, _______, _______, MG_2_ARH_R4,
+    KC_NO,   KC_VOLD, KC_LGUI, KC_LSFT, KC_BSPC, KC_LCTL, KC_LALT, KC_SPC,  TO(_QW), KC_PSCR, KC_SLCK, KC_PAUS
+  )
 };
--- users/mattgleeson/mg_keys.h
+++ users/mattgleeson/mg_keys.h
@@ -11,34 +11,62 @@
 #define MG_RTHUMB_FAR KC_BSPC
 #define MG_RTHUMB_IN MO(1)
 
+#define MG_ALH_R2 KC_Q, KC_W, KC_E, KC_R, KC_T
+#define MG_ALH_R3 MG_HOME_L
+#define MG_ALH_R4 KC_Z, KC_X, KC_C, KC_V, KC_B
+
+#define MG_ARH_R2 KC_Y, KC_U, KC_I, KC_O, KC_P
+#define MG_ARH_R3 MG_HOME_R
+#define MG_ARH_R4 KC_N, KC_M, KC_COMMA, KC_DOT, KC_SLASH
+
+/* LAYER 1 */
+#define MG_1_ALH_R2 KC_EXLM, KC_AT,   KC_LCBR, KC_RCBR, KC_PIPE
+#define MG_1_ALH_R3 _______, KC_DLR,  KC_LPRN, KC_RPRN, KC_GRAVE
+#define MG_1_ALH_R4 KC_PERC, KC_CIRC, KC_LBRC, KC_RBRC, KC_TILD
+
+#define MG_1_ARH_R2 KC_UP,   KC_7,    KC_8,    KC_9,    KC_ASTR
+#define MG_1_ARH_R3 KC_DOWN, KC_4,    KC_5,    KC_6,    _______
+#define MG_1_ARH_R4 RESET,   KC_1,    KC_2,    KC_3,    KC_BSLASH
+
+/* LAYER 2 */
+#define MG_2_ALH_R2 _______, _______, KC_MS_U, _______, _______
+#define MG_2_ALH_R3 _______, KC_MS_L, KC_MS_D, KC_MS_R, _______
+#define MG_2_ALH_R4 _______, _______, _______, _______, RESET
+
+#define MG_2_ARH_R2 KC_END,  KC_PGDN, KC_PGUP, KC_HOME, _______
+#define MG_2_ARH_R3 KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, KC_BRIGHTNESS_DOWN
+#define MG_2_ARH_R4 KC_GPM_LIKE_SONG, KC_AUDIO_VOL_DOWN, KC_AUDIO_VOL_UP, KC_MEDIA_PLAY_PAUSE, KC_MEDIA_NEXT_TRACK
+
+
 #define MG_LH_R1 KC_EQL, KC_1, KC_2, KC_3, KC_4, KC_5
-#define MG_RH_R1 KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINUS
-#define MG_LH_R2 KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T
-#define MG_RH_R2 KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BSLASH
+#define MG_LH_R2 KC_TAB, MG_ALH_R2
 #define MG_LH_R3 LCTL_T(KC_ESC), MG_HOME_L
+#define MG_LH_R4 KC_LSFT, MG_ALH_R4
+
+#define MG_RH_R1 KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINUS
+#define MG_RH_R2 MG_ARH_R2, KC_BSLASH
 #define MG_RH_R3 MG_HOME_R, KC_QUOTE
-#define MG_LH_R4 KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B
-#define MG_RH_R4 KC_N, KC_M, KC_COMMA, KC_DOT, KC_SLASH, KC_RSFT
+#define MG_RH_R4 MG_ARH_R4, KC_RSFT
 
 #define MG_1_LH_R1 KC_ESC, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5
-#define MG_1_LH_R2 KC_DEL, KC_EXLM, KC_AT, KC_LCBR, KC_RCBR, KC_PIPE
-#define MG_1_LH_R3 KC_CAPS, _______, KC_DLR, KC_LPRN, KC_RPRN, KC_GRAVE
-#define MG_1_LH_R4 _______, KC_PERCENT, KC_CIRCUMFLEX, KC_LBRACKET, KC_RBRACKET, KC_TILD
+#define MG_1_LH_R2 KC_DEL,  MG_1_ALH_R2
+#define MG_1_LH_R3 KC_CAPS, MG_1_ALH_R3
+#define MG_1_LH_R4 _______, MG_1_ALH_R4
 
 #define MG_1_RH_R1 KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11
-#define MG_1_RH_R2 KC_UP, KC_7, KC_8, KC_9, KC_ASTR, KC_F12
-#define MG_1_RH_R3 KC_DOWN, KC_4, KC_5, KC_6, _______, _______
-#define MG_1_RH_R4 RESET, KC_1, KC_2, KC_3, KC_BSLASH, _______
+#define MG_1_RH_R2 MG_1_ARH_R2, KC_F12
+#define MG_1_RH_R3 MG_1_ARH_R3, _______
+#define MG_1_RH_R4 MG_1_ARH_R4, _______
 
 #define MG_2_LH_R1 _______, _______, _______, _______, _______, _______
-#define MG_2_LH_R2 _______, _______, _______, KC_MS_UP, _______, _______
-#define MG_2_LH_R3 _______, _______, KC_MS_LEFT, KC_MS_DOWN, KC_MS_RIGHT, _______
-#define MG_2_LH_R4 _______, _______, _______, _______, _______, RESET
+#define MG_2_LH_R2 _______, MG_2_ALH_R2
+#define MG_2_LH_R3 _______, MG_2_ALH_R3
+#define MG_2_LH_R4 _______, MG_2_ALH_R4
 
 #define MG_2_RH_R1 _______, _______, _______, _______, _______, _______
-#define MG_2_RH_R2 KC_END, KC_PGDN, KC_PGUP, KC_HOME, _______, _______
-#define MG_2_RH_R3 KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT, KC_BRIGHTNESS_DOWN, KC_BRIGHTNESS_UP
-#define MG_2_RH_R4 KC_GPM_LIKE_SONG, KC_AUDIO_VOL_DOWN, KC_AUDIO_VOL_UP, KC_MEDIA_PLAY_PAUSE, KC_MEDIA_NEXT_TRACK, _______
+#define MG_2_RH_R2 MG_2_ARH_R2, _______
+#define MG_2_RH_R3 MG_2_ARH_R3, KC_BRIGHTNESS_UP
+#define MG_2_RH_R4 MG_2_ARH_R4, _______
 
 #define MG_12X4 MG_LH_R1, MG_RH_R1, MG_LH_R2, MG_RH_R2, MG_LH_R3, MG_RH_R3, MG_LH_R4, MG_RH_R4
 #define MG_1_12X4 MG_1_LH_R1, MG_1_RH_R1, MG_1_LH_R2, MG_1_RH_R2, MG_1_LH_R3, MG_1_RH_R3, MG_1_LH_R4, MG_1_RH_R4
